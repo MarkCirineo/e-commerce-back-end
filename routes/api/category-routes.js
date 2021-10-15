728c41a366171a7a619e78001c017d4b8466fbec
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-10-15 17:53:15 -0400

    added 'async' to GET route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try {
     const categoryData = await Category.findByPk(req.params.id, {
