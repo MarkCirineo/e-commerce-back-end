2311f7f580c12cdfcd113b501cd5ad42979e5acf
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-10-15 18:05:46 -0400

    finished GET route by id for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -23,9 +23,14 @@ router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag, through: ProductTag, as: "tag_products" }]
-    })
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No product found with that id" });
+      return;
+    }
+    res.status(200).json(productData);
   } catch (err) {
-    
+    res.status(500).json(err);
   }
 });
 
