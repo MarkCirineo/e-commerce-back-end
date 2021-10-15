ff6055575205931fee8f9ce6e4ca5962b3d0fd1e
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-10-15 17:38:48 -0400

    created POST route for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,7 +18,6 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
@@ -33,8 +32,14 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+  const categoryData = await Category.create(req.body);
+  res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(404).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
